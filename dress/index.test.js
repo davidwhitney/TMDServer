0e0f5cf675888e19e9df9af7f52847b952d1e889
David Whitney <david@davidwhitney.co.uk>
2019-10-19 17:16:39 +0100

    Test for fallback tweet.

--- dress/index.test.js
+++ dress/index.test.js
@@ -18,6 +18,15 @@ describe('The API', () => {
         expect(result.body).toBe('[255, 255, 255]');
     });
     
+    it('returns the first valid tweet if it\'s "hidden" by a more recent invalid one', async () => {
+        mock.response.data.push({text: '@david_whitney you\'re awesome!'});
+        mock.response.data.push({text: '@david_whitney orange'});
+
+        const result = await api({}, {});
+        
+        expect(result.body).toBe('[255, 165, 0]');
+    });
+    
     it('returns the safe default when invalid responses returned', async () => {
         mock.response = null;
 
