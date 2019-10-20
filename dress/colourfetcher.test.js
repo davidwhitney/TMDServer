b9bdfa58dde6462b0e69a8ab46230d32ca5bf723
David Whitney <david@davidwhitney.co.uk>
2019-10-20 11:42:15 +0100

    Parity with glitch

--- dress/colourfetcher.test.js
+++ dress/colourfetcher.test.js
@@ -4,7 +4,7 @@ const ThrottlingClient = require('./throttlingclient');
 
 describe('The Colour Fetcher', () => {   
     it('returns the safe default when no valid tweets are found', async () => {
-        const twitter = new ThrottlingClient(twitterResponseWith([{text: '@screen_name you\'re awesome!'}]));
+        const twitter = twitterResponseWith([{text: '@screen_name you\'re awesome!'}]);
         const sut = new ColourFetcher(twitter, colours, "screen_name");
 
         const result = await sut.execute();
@@ -13,10 +13,10 @@ describe('The Colour Fetcher', () => {
     });
     
     it('returns the first valid tweet if it\'s "hidden" by a more recent invalid one', async () => {        
-        const twitter = new ThrottlingClient(twitterResponseWith([
+        const twitter = twitterResponseWith([
             {text: '@screen_name you\'re awesome!'},
             {text: '@screen_name orange'}
-        ]));
+        ]);
         const sut = new ColourFetcher(twitter, colours, "screen_name");
 
         const result = await sut.execute();
@@ -25,7 +25,7 @@ describe('The Colour Fetcher', () => {
     });
     
     it('returns the safe default when invalid responses returned', async () => {
-        const twitter = new ThrottlingClient(twitterResponseWith(null));
+        const twitter = twitterResponseWith(null);
         const sut = new ColourFetcher(twitter, colours, "screen_name");
 
         const result = await sut.execute();
@@ -34,7 +34,7 @@ describe('The Colour Fetcher', () => {
     });
 
     it('when tweeted a known colour, responds with the hex code', async () => {
-        const twitter = new ThrottlingClient(twitterResponseWith([{text: '@screen_name orange'}]));
+        const twitter = twitterResponseWith([{text: '@screen_name orange'}]);
         const sut = new ColourFetcher(twitter, colours, "screen_name");
 
         const result = await sut.execute();
@@ -50,8 +50,6 @@ const twitterResponseWith = (data) => {
             resp: { headers: { 'x-rate-limit-reset': '1571496333', 'x-rate-limit-remaining': '100' } },
             data: data
         },
-        get: async function(path, query) { 
-            return this.response;
-        }
+        get: async function(path, query) { return this.response; }
     };
 }
